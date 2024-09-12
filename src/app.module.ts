f326d4fd9bdb9b5b2895d232fe40f1693eebf770
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-09-12 16:52:42 -0600

    feat: Add ChurchesModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
+import { ChurchesModule } from './churches/churches.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, EmailModule],
+  imports: [UsersModule, AuthModule, EmailModule, ChurchesModule],
   controllers: [],
   providers: [],
 })
